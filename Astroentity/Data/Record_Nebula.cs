c95a5baa462c821d984854c107a281eb9f1cb75f
ispeedonthe405 <ispeedonthe405@gmail.com>
2025-02-07 12:23:28 -0800

    Begin adding fields to record types

--- Astroentity/Data/Record_Nebula.cs
+++ Astroentity/Data/Record_Nebula.cs
@@ -1,6 +1,16 @@
-﻿namespace Astroentity.Data
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Astroentity.Data
 {
     internal partial class Record_Nebula : Record_Base
     {
+        [ObservableProperty]
+        public string name = string.Empty;
+
+        [ObservableProperty]
+        public string description = string.Empty;
+
+        [ObservableProperty]
+        public double distance;
     }
 }
--- Astroentity/Data/Record_Star.cs
+++ Astroentity/Data/Record_Star.cs
@@ -1,6 +1,19 @@
-﻿namespace Astroentity.Data
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Astroentity.Data
 {
     internal partial class Record_Star : Record_Base
     {
+        [ObservableProperty]
+        public string name = string.Empty;
+
+        [ObservableProperty]
+        public string description = string.Empty;
+
+        [ObservableProperty]
+        public double distance;
+
+        [ObservableProperty]
+        public string stellarClass = string.Empty;
     }
 }
